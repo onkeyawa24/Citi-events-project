f549e9b4a2736b354e3c404ac13d79e521642789
Onke Yawa <42420810+onkeyawa24@users.noreply.github.com>
2025-07-08 09:52:25 +0200

    Update App.js

--- citi-events/src/App.js
+++ citi-events/src/App.js
@@ -3,12 +3,12 @@ import React from 'react';
 import EventsPage from './EventsPage';
 import UploadForm from './UploadForm';
 import MotivationPage from './MotivationPage';
-import NavbarPage from './NavbarPage';
+
+
 
 function App() {
   return (
-    <div style={{ padding: '80px', fontFamily: 'Arial, sans-serif' }}>
-   
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
       <h1>📅 Citi-events</h1>
       <MotivationPage />
        <hr style={{ margin: '2rem 0' }} />
