a1c0a6e115651702328918e76801189099e15a45
Onke Yawa <42420810+onkeyawa24@users.noreply.github.com>
2025-07-21 10:16:44 +0200

    Add files via upload

---
+++ citi-events/src/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "frontend2a481508": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
