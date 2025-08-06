d572d10e7be6993aed37277ed252f65da5bedd5c
Onke Yawa <42420810+onkeyawa24@users.noreply.github.com>
2025-08-06 11:17:56 +0200

    Update amplifyConfig.js

--- citi-events/src/amplifyConfig.js
+++ citi-events/src/amplifyConfig.js
@@ -3,7 +3,8 @@ import { Amplify } from 'aws-amplify';
 Amplify.configure({
   Auth: {
     region: 'eu-north-1',
-    userPoolId: 'eu-north-1_qorUsrUD4', // Replace if different
-    userPoolWebClientId: '17suc0fv4d86tt8i5dd9phjd0gi', // Replace if different
+    userPoolId: 'eu-north-1_qorUsrUD4', 
+    userPoolWebClientId: '17suc0fv4d86tt8i5dd9phjd0gi', 
   }
 });
+
